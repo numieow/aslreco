99dca2b8e76431a9e69913f5596ae67782d00a8e
WIRTH Maxime <maxime.wirth@telecomnancy.eu>
2023-03-22 16:53:13 +0100

    [+] Début prétraitement

--- recognition.py
+++ recognition.py
@@ -8,5 +8,21 @@ import scipy as sp
 import pandas as pd
 
 #Importing data from data/sign_mnist_train.csv
-df1 = pd.read_csv('data\sign_mnist_train.csv', delimiter=',')
-print(df1)
+training = pd.read_csv('data\sign_mnist_train.csv', delimiter=',')
+testing = pd.read_csv('data\sign_mnist_test.csv', delimiter=',')
+
+inputs_train = training.iloc[:, 1:].to_numpy()
+targets_train = training['label'].to_numpy()
+
+inputs_test = testing.iloc[:, 1:].to_numpy()
+targets_test = testing['label'].to_numpy()
+
+#The different labels
+print(inputs_train['label'].unique())
+
+#Normalize inputs
+inputs_train = inputs_train / 255.0
+inputs_test = inputs_test / 255.0
+
+
+
