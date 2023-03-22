c1c95104c7344d9a182ac931728d4e001c4ec12c
WIRTH Maxime <maxime.wirth@telecomnancy.eu>
2023-03-22 16:42:46 +0100

    [+] Extraction des données

--- recognition.py
+++ recognition.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
+from tensorflow.python import keras
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn as skl
+import scipy as sp
+import pandas as pd
 
-print(tf.__version__)
+#Importing data from data/sign_mnist_train.csv
+df1 = pd.read_csv('data\sign_mnist_train.csv', delimiter=',')
+print(df1)
